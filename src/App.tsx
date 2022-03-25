ffa5e697a712773512985f08fa09d7eb1c27cf45
khoi <khoidolce@gmail.com>
2022-03-25 10:40:29 +0700

    update: homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { NavLink, Route, Routes } from 'react-router-dom'
+import WebsiteLayout from './pages/layouts/WebsiteLayout'
+import HomePage from './pages/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-
+      <header>
+        <ul>
+          <li><NavLink to="/">HomePage</NavLink></li>
+          <li><NavLink to="/product">Product</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+        </ul>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<WebsiteLayout />}>
+            <Route index element={<HomePage />} />
+          </Route>
+        </Routes>
+      </main>
     </div>
   )
 }
---
+++ src/pages/layouts/WebsiteLayout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const WebsiteLayout = (props: Props) => {
+    return (
+        <div>
+            <header></header>
+            <main></main>
+            <footer></footer>
+        </div>
+    )
+}
+
+export default WebsiteLayout
