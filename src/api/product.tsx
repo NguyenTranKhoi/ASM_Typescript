120e79636c3197a509fcf90978927baaa4ef0229
khoi <khoidolce@gmail.com>
2022-03-28 11:05:56 +0700

    feact: api

--- src/api/product.tsx
+++ src/api/product.tsx
@@ -1,22 +1,30 @@
+import { ProductType } from "../types/product";
+import { isAuthenticate } from "../utils/localStorage";
 import instance from "./instance";
+
+const { token, user } = isAuthenticate();
 
 export const list = () => {
     const url = `/products`;
     return instance.get(url)
 }
-export const add = (product: any) => {
-    const url = `/products`;
-    return instance.post(url, product)
+export const add = (product: ProductType) => {
+    const url = `/products/${user._id}`;
+    return instance.post(url, product, {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    });
 }
-export const remove = (id: any) => {
+export const remove = (id: number) => {
     const url = `/products/${id}`;
     return instance.delete(url)
 }
-export const read = (id: any) => {
+export const read = (id: string | undefined) => {
     const url = `/products/${id}`;
     return instance.get(url)
 }
-export const update = (product: any) => {
+export const update = (product: ProductType) => {
     const url = `/products/${product.id}`;
     return instance.put(url, product);
 }
