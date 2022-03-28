0324717bc4744a7ea066999a0e9e92bf0a29f0e7
khoi <khoidolce@gmail.com>
2022-03-28 13:00:37 +0700

    feact: admin layout

--- src/pages/layouts/AdminLayout.tsx
+++ src/pages/layouts/AdminLayout.tsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import AdminNav from '../../components/admin/AdminNav'
 
 type Props = {}
 
 const AdminLayout = (props: Props) => {
     return (
         <div>
-            <header>
-                Admin
-            </header>
-            <div className="row">
-                <div className="col-2">
-                    <aside>Sidebar</aside>
-                </div>
-                <div className="col-10">
-                    <main>
-                        <Outlet />
-                    </main>
-                </div>
+            <div className="min-h-full">
+                <AdminNav />
+                <header className="bg-white shadow">
+                    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+                    </div>
+                </header>
+                <main>
+                    <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                        {/* Replace with your content */}
+                        <div className="px-4 py-6 sm:px-0">
+                            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+                        </div>
+                        {/* /End replace */}
+                    </div>
+                </main>
             </div>
         </div>
     )
