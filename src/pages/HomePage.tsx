a179b490eb4a9a9382f5f95d55304db1ad00b97e
khoi <khoidolce@gmail.com>
2022-04-05 00:21:36 +0700

    feact: home img

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -30,17 +30,17 @@ const HomePage = (props: Props) => {
             <main>
                 <div className="content-top grid grid-cols-4 pt-[50px] gap-[20px] mx-[40px]">
                     {products.map((item, index) => {
-                        return <div className="w-full content-top-product border-dashed border-2 border-slate-400 text-center m-auto h-[370px] pt-[10px]" key={index}>
+                        return <div className="w-full content-top-product border-dashed border-2 border-slate-300 text-center m-auto h-[370px] pt-[10px] rounded-[10px]" key={index}>
                             <div className="hihi overflow-hidden">
                                 <a href="#">
-                                    {/* <img src="${pro.img}" width="{200}" className="hover:scale-110 duration-700 m-auto" /> */}
+                                    <img src={`${item.img}`} width="200" className="hover:scale-110 duration-700 m-auto" />
                                 </a>
                             </div>
                             <p className="pt-[10px] text-sm text-slate-500" >{item.nameextra}</p>
                             <Link to={`/products/${item._id}`}>
-                                <h4 className="text-base"><a href="" className="no-underline text-stone-900 hover:text-amber-700">{item.name}</a></h4>
+                                <h4 className="text-lg font-semibold pt-[5px]"><a href="" className="no-underline text-stone-900 hover:text-amber-700">{item.name}</a></h4>
                             </Link>
-                            <div className="text">
+                            <div className="text pt-[10px]">
                                 <del className="pr-[15px]">{formatter.format(item.pricedrop)}</del>
                                 <strong>{formatter.format(item.price)}</strong>
                             </div>
