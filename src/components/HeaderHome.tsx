c0576cda0ec97cd48c185c1b6b8b79b108803f6a
khoi <khoidolce@gmail.com>
2022-04-05 19:47:37 +0700

    feact: hedaer

--- src/components/HeaderHome.tsx
+++ src/components/HeaderHome.tsx
@@ -28,10 +28,10 @@ const HeaderHome = (props: Props) => {
                         <a href="/"><img src="../img/logo-mona-watches-white.png" width="130" /></a>
                     </div>
                     <div className="icon pt-[5px] mr-[35px]">
-                        <div className="box h-[40px] absolute top-[26%] right-[13%] translate-[-50%,-50%] bg-teal-100">
+                        <div className="box h-[35px] absolute top-[26%] right-[13%] translate-[-50%,-50%] bg-white rounded-[50px] p-[6px]">
                             <form className="sbox" action="/search" method="get">
-                                <input className="stext" type="text" name="q" placeholder="Tìm kiếm bài viết..." />
-                                <a className="sbutton" type="submit" href="javascript:void(0);">
+                                <input className="stext border-none outline-none" type="text" name="q" placeholder="Tìm kiếm bài viết..." />
+                                <a className="sbutton text-black float-right w-[40px] h-[40px] rounded-[50%] " type="submit" href="javascript:void(0);">
                                     <i className="fa fa-search" />
                                 </a>
                             </form>
@@ -47,9 +47,11 @@ const HeaderHome = (props: Props) => {
                                     </div>
                                 </Link> :
                                 <Link to={'/Signin'}>
-                                    <a href="" className="no-underline text-slate-50 hover:text-orange-500 p-[10px]">
-                                        Signin
-                                    </a>
+                                    <div className="pt-[10px]">
+                                        <a href="" className="no-underline text-slate-50 hover:text-orange-500 p-[10px]">
+                                            Signin
+                                        </a>
+                                    </div>
                                 </Link>
                             }
                             <i className="fas fa-cart-plus text-slate-50 hover:text-orange-500 p-[10px] pt-[12px]"></i>
