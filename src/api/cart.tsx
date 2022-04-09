6cd36fb44a9f29440c37d3ba5514296044dc1b7c
khoi <khoidolce@gmail.com>
2022-04-09 10:02:52 +0700

    feact: api cart

---
+++ src/api/cart.tsx
@@ -0,0 +1,19 @@
+import { TypeCart } from "../types/cart"
+import instance from "./instance"
+
+export const read = (id: string | undefined) => {
+    const url = `/cart/${id}`
+    return instance.get(url)
+}
+export const listcart = () => {
+    const url = `/cart`
+    return instance.get(url)
+}
+export const addcart = (productcart: TypeCart) => {
+    const url = `/cart`
+    return instance.post(url, productcart)
+}
+export const removecart = (id: number) => {
+    const url = `/cart/${id}`
+    return instance.delete(url)
+}
---
+++ src/types/cart.tsx
@@ -0,0 +1,8 @@
+export type TypeCart = {
+    _id?: number,
+    name: string,
+    img: string,
+    price: number,
+    user: string,
+    quantiny: number
+}
